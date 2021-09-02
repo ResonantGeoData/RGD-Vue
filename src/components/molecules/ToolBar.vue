7ef0b82916e4ad8295bacef36e496e4d4acad572
Mary Salvi <mary.salvi@narok.khq.kitware.com>
2021-09-02 10:26:40 -0400

    start of sidebar and app structure

---
+++ src/components/molecules/ToolBar.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'ToolBar',
+  props: {
+    items: {
+      type: Array,
+      required: false,
+    },
+    text: {
+      type: String,
+      required: false,
+    },
+  },
+  inheritAttrs: false,
+});
+
+</script>
+
+<template>
+  <div>
+    <v-toolbar
+      v-if="items"
+      v-bind="$attrs"
+    >
+      <v-tabs>
+        <v-tab
+          v-for="item in items"
+          :key="item"
+        >
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+    <v-toolbar
+      v-else-if="text"
+      v-bind="$attrs"
+    >
+      {{ text }}
+    </v-toolbar>
+    <v-toolbar
+      v-else
+      v-bind="$attrs"
+    />
+  </div>
+</template>
---
+++ src/components/organisms/SearchBar.vue
@@ -0,0 +1,67 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import ToolBar from '../molecules/ToolBar.vue';
+
+export default defineComponent({
+  name: 'SearchBar',
+  components: {
+    ToolBar,
+  },
+  setup() {
+    const tabData = ['LatLong', 'Bounding Box', 'GeoJson'];
+    const text = 'Other Params';
+
+    return {
+      tabData,
+      text,
+    };
+  },
+});
+</script>
+<template>
+  <v-card
+    color="blue-grey darken-4"
+    height="1200px"
+  >
+    <ToolBar
+      color="blue-grey darken-2"
+      flat
+    />
+    <v-card-title>
+      Sample Project
+    </v-card-title>
+    <ToolBar
+      :items="tabData"
+      color="blue-grey darken-2"
+      flat
+    />
+    <v-row no-gutters>
+      <v-col class="ma-1">
+        <v-text-field
+          label="Longitude(deg)"
+          outlined
+        />
+      </v-col>
+      <v-col class="ma-1">
+        <v-text-field
+          label="Latitdue(deg)"
+          outlined
+        />
+      </v-col>
+      <v-col
+        cols="10"
+        class="pl-1 pr-4"
+      >
+        <v-text-field
+          label="Radius (m)"
+          outlined
+        />
+      </v-col>
+    </v-row>
+    <ToolBar
+      :text="text"
+      color="blue-grey darken-2"
+      flat
+    />
+  </v-card>
+</template>
