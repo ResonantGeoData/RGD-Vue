9feaf1881108b1088e95889a9aa30b52f4a68943
Mary Salvi <mary.salvi@delaroid.khq.kitware.com>
2021-09-30 12:36:03 -0400

    updates cesiumViewer to work with input

--- src/components/organisms/CesiumViewer.vue
+++ src/components/organisms/CesiumViewer.vue
@@ -1,19 +1,52 @@
 <script lang="ts">
-import { defineComponent, onMounted, ref } from '@vue/composition-api';
+import {
+  defineComponent,
+  onMounted,
+  ref,
+  toRef,
+  watch,
+}
+  from '@vue/composition-api';
 
 import Cesium from '@/plugins/cesium';
 
 export default defineComponent({
   name: 'CesiumViewer',
 
-  setup() {
-    const cesiumViewer = ref(null);
+  props: {
+    location: {
+      type: Object, /* TODO typescript interfaces for props */
+      required: false,
+      default: () => ({}),
+    },
+  },
+
+  setup(props) {
+    const cesiumViewer = ref();
 
     onMounted(() => {
-      cesiumViewer.value = new Cesium.Viewer('cesiumContainer');
+      cesiumViewer.value = new Cesium.Viewer('cesiumContainer', {
+        animation: false,
+        timeline: false,
+      });
+      cesiumViewer.value.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+      });
     });
 
-    return {};
+    const updateCamera = () => {
+      cesiumViewer.value.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(
+          props.location.longitude,
+          props.location.latitude,
+          props.location.height,
+        ),
+      });
+    };
+
+    watch(toRef(props, 'location'), updateCamera, {
+      deep: true,
+    });
   },
 
 });
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import OauthClient from '@girder/oauth-client';
 import * as Sentry from '@sentry/vue';
 import Vue from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
+import Cesium from './plugins/cesium';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
 
 Vue.use(VueCompositionAPI);
+
+Cesium.Ion.defaultAccessToken = process.env.VUE_APP_CESIUM_ACCESS_TOKEN;
 
 const axiosInstance = axios.create({
   baseURL: process.env.VUE_APP_API_ROOT,
