1b808f691a5948ca430ef198d59249b61762523c
Mary Salvi <40494088+marySalvi@users.noreply.github.com>
2021-10-06 13:37:17 -0400

    updates cesiumViewer to work with input (#9)
    * Webpack config for CesiumJS, remove VueCesium
    
    * Instantiate cesium viewer
    
    * Bundle cesium build files instead of source
    
    * updates cesiumViewer to work with input
    
    Co-authored-by: Mike VanDenburgh <michael.vandenburgh@kitware.com>
    Co-authored-by: Mary Salvi <mary.salvi@delaroid.khq.kitware.com>

--- src/components/organisms/CesiumViewer.vue
+++ src/components/organisms/CesiumViewer.vue
@@ -1,23 +1,47 @@
 <script lang="ts">
-import { defineComponent, onMounted, ref } from '@vue/composition-api';
-
+import {
+  defineComponent,
+  onMounted,
+  ref,
+  toRef,
+  watch,
+}
+  from '@vue/composition-api';
 import Cesium from '@/plugins/cesium';
-
 export default defineComponent({
   name: 'CesiumViewer',
-
-  setup() {
-    const cesiumViewer = ref(null);
-
-    onMounted(() => {
-      cesiumViewer.value = new Cesium.Viewer('cesiumContainer');
-    });
-
-    return {};
+  props: {
+    location: {
+      type: Object, /* TODO typescript interfaces for props */
+      required: false,
+      default: () => ({}),
+    },
   },
-
+  setup(props) {
+    const cesiumViewer = ref();
+    onMounted(() => {
+      cesiumViewer.value = new Cesium.Viewer('cesiumContainer', {
+        animation: false,
+        timeline: false,
+      });
+      cesiumViewer.value.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+      });
+    });
+    const updateCamera = () => {
+      cesiumViewer.value.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(
+          props.location.longitude,
+          props.location.latitude,
+          props.location.height,
+        ),
+      });
+    };
+    watch(toRef(props, 'location'), updateCamera, {
+      deep: true,
+    });
+  },
 });
-
 </script>
 <template>
   <div id="cesiumContainer" />
