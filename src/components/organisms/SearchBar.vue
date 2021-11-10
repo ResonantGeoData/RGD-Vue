95e49a2186e66033e4d7f9e06d619497444d5217
Mary Salvi <mary.salvi@kitware.com>
2021-11-10 16:24:57 -0500

    add results table view

--- src/components/organisms/SearchBar.vue
+++ src/components/organisms/SearchBar.vue
@@ -1,8 +1,10 @@
 <script lang="ts">
 import {
-  defineComponent, Ref, ref,
+  defineComponent, Ref, ref, reactive,
 } from '@vue/composition-api';
 import { Parameters } from '@/store/types';
+import { rgdSearch } from '@/api/rest';
+import type { DataOptions } from 'vuetify';
 import ToolBar from '../molecules/ToolBar.vue';
 import TabToolBar from '../molecules/TabToolBar.vue';
 import GeoJsonForm from '../molecules/GeoJsonForm.vue';
@@ -41,15 +43,42 @@ export default defineComponent({
         endTime: null,
       },
     });
+    const tableOptions = reactive({
+      page: 1,
+      sortBy: ['spatial_id'],
+      sortDesc: [true],
+    } as DataOptions);
+
+    const headers = [
+      { text: 'ID-Name', value: 'spatial_id' },
+      // { text: 'Area', value: '' },
+      { text: 'Data Type', value: 'subentry_type' },
+    ];
+
+    const searchResults = ref();
+
+    const reveal = false;
+
+    const updateResults = async () => {
+      const res = await rgdSearch();
+      searchResults.value = res.data.results;
+      console.log(searchResults.value);
+    };
+    console.log(reveal);
+    // updateResults();
 
     return {
       params,
+      tableOptions,
+      headers,
+      searchResults,
+      updateResults,
+      reveal,
     };
 
     // Will be needed in second iteration
     // const tabData = ['LatLong', 'Bounding Box', 'GeoJson'];
     // const activeTab = ref(0);
-    test();
   },
 });
 </script>
@@ -60,53 +89,97 @@ export default defineComponent({
   >
     <v-card-title>
       Sample Project
+      <v-spacer />
+      <v-card-actions>
+        <v-btn
+          v-if="searchResults"
+          plain
+          @click="reveal=true"
+        >
+          Show Results
+        </v-btn>
+      </v-card-actions>
     </v-card-title>
-    <!-- Will be needed in second iteration -->
-    <!-- <TabToolBar
+    <v-expand-transition>
+      <v-card
+        v-if="reveal"
+        class="transition-fast-in-fast-out v-card--reveal"
+        style="height: 100%;"
+      >
+        <v-data-table
+          :headers="headers"
+          :items="searchResults"
+        />
+        <v-card-actions class="pt-0">
+          <v-btn
+            text
+            color="teal accent-4"
+            @click="reveal = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+
+      <!-- Will be needed in second iteration -->
+      <!-- <TabToolBar
       v-model="activeTab"
       :items="tabData"
       color="blue-grey darken-2"
       flat
     /> -->
-    <v-form @submit.prevent="$emit('input', inputs)">
-      <!-- Will be needed in second iteration -->
-      <!-- <LatLongForm
+      <v-form
+        @submit.prevent="updateResults"
+      >
+        <!-- Will be needed in second iteration -->
+        <!-- <LatLongForm
         v-if="activeTab === 0"
       />
       <BoundingBoxForm
         v-if="activeTab === 1"
       /> -->
-      <ToolBar
-        text="Geo Json"
-        color="blue-grey darken-2"
-        flat
-      />
-      <GeoJsonForm />
-      <ToolBar
-        text="Other Params"
-        color="blue-grey darken-2"
-        flat
-      />
-      <OtherParams
-        v-model="params"
-      />
-      <v-row
-        no-gutters
-        justify="center"
-      >
-        <v-col
-          cols="11"
+        <ToolBar
+          text="Geo Json"
+          color="blue-grey darken-2"
+          flat
+        />
+        <GeoJsonForm />
+        <ToolBar
+          text="Other Params"
+          color="blue-grey darken-2"
+          flat
+        />
+        <OtherParams
+          v-model="params"
+        />
+        <v-row
+          no-gutters
+          justify="center"
         >
-          <v-btn
-            color="teal accent-4"
-            block
-            x-large
-            class="mt-3"
+          <v-col
+            cols="11"
           >
-            Search
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-form>
+            <v-btn
+              color="teal accent-4"
+              block
+              x-large
+              type="submit"
+              class="mt-3"
+            >
+              Search
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-expand-transition>
   </v-card>
 </template>
+
+<style>
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
+</style>
