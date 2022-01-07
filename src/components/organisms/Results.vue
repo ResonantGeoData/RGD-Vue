ad5e06f0ac728d90b641f28257a1f14a5a3963d2
Anne Haley <anne.haley@kitware.com>
2022-01-07 16:57:07 -0500

    Put icons back and remove result link

--- src/components/organisms/Results.vue
+++ src/components/organisms/Results.vue
@@ -21,16 +21,22 @@ export default defineComponent({
 
     const headers = [
       { text: 'ID-Name', value: 'id-name' },
-      { text: 'Data Type', value: 'subentry_type' },
+      {
+        text: 'Data Type', value: 'subentry_type', align: 'center', width: 2,
+      },
     ];
 
-    const baseLink = `${process.env.VUE_APP_API_ROOT}rgd/spatial_entries/`;
-
+    const ellipsisText = (str: string) => {
+      if (str.length > 15) {
+        return `${str.substr(0, 10)}...${str.substr(str.length - 5, str.length)}`;
+      }
+      return str;
+    };
     return {
       searchResults,
       tableOptions,
       headers,
-      baseLink,
+      ellipsisText,
     };
   },
 });
@@ -43,31 +49,40 @@ export default defineComponent({
     <v-data-table
       :headers="headers"
       :items="searchResults"
-      class="resultsTable"
       dense
+      calculate-widths
     >
       <!-- eslint-disable-next-line -->
-     <template #item.id-name="{item}">
-        <div
-          class="text-truncate"
-          style="max-width: 10vw;"
-        >
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
-              <a
-                :href="`${baseLink}${item.spatial_id}`"
-                v-bind="attrs"
-                v-on="on"
-              >
-                {{ item.spatial_id }}{{ item.subentry_name ? ` - ${item.subentry_name}` : '' }}
-              </a>
-            </template>
-            <span>
-              View {{ item.spatial_id }}{{ item.subentry_name ? ` - ${item.subentry_name}` : '' }}
-            </span>
-          </v-tooltip>
+      <template #item.id-name="{item}">
+        <div style="max-width: 10vw;">
+          {{ item.spatial_id }}
+          {{ item.subentry_name ? ` - ${ellipsisText(item.subentry_name)}` : '' }}
         </div>
+      </template>
+      <!-- eslint-disable-next-line -->
+      <template #item.subentry_type="{item}">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <img
+              v-bind="attrs"
+              :src="`${item.subentry_type.toLowerCase()}.png`"
+              :alt="item.subentry_type"
+              class="data-type-icon"
+              v-on="on"
+            >
+          </template>
+          <span>
+            {{ item.subentry_type }}
+          </span>
+        </v-tooltip>
       </template>
     </v-data-table>
   </div>
 </template>
+
+<style scoped>
+.data-type-icon {
+  max-height: 25px;
+  filter: invert(100%);
+}
+</style>
