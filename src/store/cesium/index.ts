302585ae520ed15027d6413f8d1cd189aa65a522
banesullivan <bane.sullivan@kitware.com>
2022-01-31 10:32:33 -0700

    Use proper coloring for sites

--- src/store/cesium/index.ts
+++ src/store/cesium/index.ts
@@ -11,6 +11,20 @@ Cesium.RequestScheduler.maximumRequestsPerServer = 3;
 
 export const cesiumViewer = ref();
 
+/* eslint-disable @typescript-eslint/camelcase */
+// See also https://gitlab.kitware.com/smart/watch/-/blob/master/watch/heuristics.py#L45
+const HUERISTIC_STATUS_DATA = {
+  positive_annotated: '#808000', // olive
+  positive_partial: '#32CD32', // limegreen
+  positive_pending: '#2E8B57', // seagreen
+  positive_excluded: '#006400', // darkgreen
+  positive_unbounded: '#4682B4', // steelblue
+  negative: '#FF4500', // orangered
+  negative_unbounded: '#FF1493', // deeppink
+  ignore: '#800080', // purple
+};
+/* eslint-enable no-param-reassign */
+
 export const addGeojson = async (geojson: GeoJSON): Promise<GeoJsonDataSource> => {
   // cesiumViewer.value.dataSources.remove(source);
   const source = await cesiumViewer.value.dataSources.add(
@@ -19,11 +33,20 @@ export const addGeojson = async (geojson: GeoJSON): Promise<GeoJsonDataSource> =
   // Change display properties for all entities in data source
   /* eslint-disable no-param-reassign */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  source.entities.values.forEach((entity: {polygon: any}) => {
-    entity.polygon.outline = true;
-    entity.polygon.fill = false;
-    entity.polygon.outlineColor = Cesium.Color.fromRandom({ alpha: 0.5 });
-    entity.polygon.outlineWidth = 3; // WebGL issue - doesn't do anything
+  source.entities.values.forEach((entity: {polygon: any; properties: any}) => {
+    let color;
+    if (entity.properties?.status && entity.properties.status?._value in HUERISTIC_STATUS_DATA) {
+      const status = entity.properties.status?._value as keyof typeof HUERISTIC_STATUS_DATA;
+      const colorV = HUERISTIC_STATUS_DATA[status];
+      color = Cesium.Color.fromCssColorString(colorV).withAlpha(0.25);
+    } else {
+      color = Cesium.Color.fromRandom().withAlpha(0.25);
+    }
+    entity.polygon.height = 0;
+    entity.polygon.material = color;
+    entity.polygon.outlineColor = Cesium.Color.BLACK;
+    // entity.polygon.outline = true;
+    // entity.polygon.outlineWidth = 30; // WebGL issue - doesn't do anything
   });
   /* eslint-enable no-param-reassign */
 
